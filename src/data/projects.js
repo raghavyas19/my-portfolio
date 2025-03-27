a949dab3d38152db021f2bcfef5b3ed5715a42a3
raghavyas19 <raghasvyas@gmail.com>
2025-03-28 04:36:16 +0530

    added images

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "Loan Management App",
     description: "A fintech Loan Management System enabling seamless loan processing and secure banking API integration. Currently in development.",
     technologies: ["React", "Node.js", "MongoDB"],
-    imageUrl: "dist/assets/LMS.png",
+    imageUrl: "/images/lms.png",
     githubUrl: "https://github.com/raghavyas19/CASH-N-TECH",
     liveUrl: "https://cash-n-tech.vercel.app/homescreen"
   },
@@ -13,7 +13,7 @@ export const projects = [
     title: "Sorting Algo. Visualizer",
     description: "Developed a Sorting Visualizer with HTML, CSS, JS, and Bootstrap, featuring an interactive UI to visualize sorting algorithms in real time.",
     technologies: ["C++"],
-    imageUrl: "dist/assets/Sorting.png",
+    imageUrl: "/images/sorting",
     githubUrl: "https://github.com/yourusername/task-manager",
     liveUrl:"https://raghavyas19.github.io/Sorting-Visualizer/"
   },
@@ -22,7 +22,7 @@ export const projects = [
     title: "To-Do List App",
     description: "A simple and efficient To-Do List App to manage your tasks effortlessly.",
     technologies: ["HTML", "CSS", "JavaScript"],
-    imageUrl: "dist/assets/ToDo.jpg",
+    imageUrl: "/images/todo.jpg",
     githubUrl: "/",
     liveUrl: "/"
   }
